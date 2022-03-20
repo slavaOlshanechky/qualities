4e0c01a0b7a4154746f1ad69c67ca1723a2a6586
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-03-21 00:29:08 +0200

    add config.json axios.defaults.baseUrl

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from "react";
 import EditForm from "../components/ui/editForm";
 import {useParams} from "react-router-dom";
 import httpService from "../services/http.service";
+import config from '../config.json'
 
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null)
     const id = useParams().id
-    const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+    // const qualityEndPoint = config.apiEndPoint + `quality/${id}`
+    const qualityEndPoint = `quality/${id}`
 
     const handleSubmit = async (data) => {
         try {
