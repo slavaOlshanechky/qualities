9f89ea4fde71a02bab80000ee7189047b4f58f58
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-03-21 15:23:50 +0200

    add quality.service

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,28 +1,41 @@
 import React, {useEffect, useState} from "react";
 import EditForm from "../components/ui/editForm";
 import {useParams} from "react-router-dom";
-import httpService from "../services/http.service";
-import config from '../config.json'
+import qualityService from "../services/quality.service";
+import {toast} from "react-toastify";
 
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null)
+    // const [errors, setErrors] = useState(null)
     const id = useParams().id
-    // const qualityEndPoint = config.apiEndPoint + `quality/${id}`
-    const qualityEndPoint = `quality/${id}`
 
-    const handleSubmit = async (data) => {
+    const updateQuality = async (content) => {
         try {
-            await httpService
-                .put(qualityEndPoint, data)
-                .then((res) => console.log(res.data.content))
+            const data = await qualityService.update(id, content)
+            return data.content
         } catch (error) {
-            console.log("Expected error")
+            const {message} = error.response.data
+            // setErrors({message, code})
+            toast.error(message)
         }
     }
+    const getQuality = async (id) => {
+        try {
+            const data = await qualityService.get(id)
+            return data.content
+        } catch (error) {
+            const {message} = error.response.data
+            toast.error(message)
+        }
+    }
+    const handleSubmit = (data) => {
+        updateQuality(data)
+    }
 
-    useEffect(async () => {
-        const {data} = await httpService.get(qualityEndPoint)
-        setQuality(data.content)
+    useEffect(() => {
+        getQuality(id).then((data) => {
+            setQuality(data.content)
+        })
     }, [])
 
     return (
